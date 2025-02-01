0ad5ce4b029ec89183c13643376c28f5e73fd04d
david-dya4yk <daviddya4yk@mail.ru>
2025-02-02 00:55:45 +0200

    fsdf

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <h1 className="text-3xl">NAVBAR</h1>
+      <h1 className="text-3xl">Layout</h1>
       {children}
     </div>
   );
