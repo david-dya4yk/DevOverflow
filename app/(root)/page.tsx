ad97798603a350cca1602b0e174d2eade369d6fb
david-dya4yk <daviddya4yk@mail.ru>
2025-02-05 03:34:59 +0200

    add social auth forms, layout auth, auth with github and google providers

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,23 @@
-const Home = () => {
+import { auth, signOut } from "@/auth";
+import { Button } from "@/components/ui/button";
+import ROUTES from "@/constants/routes";
+
+const Home = async () => {
+  const session = await auth();
+
+  console.log(session);
   return (
     <>
       <h1 className="h1-bold font-space-grotesk">Welcome next-js</h1>
+      <form
+        className="px-10 pt-[100px]"
+        action={async () => {
+          "use server";
+          await signOut({ redirectTo: ROUTES.SIGN_IN });
+        }}
+      >
+        <Button type="submit">log out</Button>
+      </form>
     </>
   );
 };
