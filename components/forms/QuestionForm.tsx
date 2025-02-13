41eac31d10791b865d1279ca7d9b15d3b6b8e75b
david-dya4yk <daviddya4yk@mail.ru>
2025-02-13 13:22:33 +0200

    implomented inputs in ask question page

---
+++ components/forms/QuestionForm.tsx
@@ -0,0 +1,120 @@
+"use client";
+
+import React from "react";
+import { AskQuestionSchema } from "@/lib/validations";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../ui/form";
+import { Input } from "../ui/input";
+import { title } from "process";
+import { Button } from "../ui/button";
+
+const QuestionForm = () => {
+  const form = useForm({
+    resolver: zodResolver(AskQuestionSchema),
+    defaultValues: {
+      title: "",
+      content: "",
+      tags: [],
+    },
+  });
+
+  const handleCreateQuestion = () => {};
+
+  return (
+    <Form {...form}>
+      <form
+        className="gap-10 flex w-full flex-col"
+        onSubmit={form.handleSubmit(handleCreateQuestion)}
+      >
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Question title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  className="paragraph-regular background-light700_dark300 light-border-2 
+                    text-dark300_light700 no-focus min-h-[56px]  border"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2.5">
+                Be specific and imagine you’re asking a question to another
+                person.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="title"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Detailed explanation of your problem?
+                <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl></FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2.5">
+                Introduce the problem and expand on what you put in the title.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="paragraph-semibold text-dark400_light800">
+                Tags <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl>
+                <div>
+                  <Input
+                    placeholder="Add tags..."
+                    type="text"
+                    className="paragraph-regular background-light700_dark300 light-border-2 
+                  text-dark300_light700 no-focus min-h-[56px]  border"
+                    {...field}
+                  />
+                  Tags
+                </div>
+              </FormControl>
+              <FormDescription className="body-regular text-light-500 mt-2.5">
+                Add up to 5 tags to describe what your question is about. Start
+                typing to see suggestions.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="mt-16 flex justify-end">
+          <Button
+            className="primary-gradient !text-light-900 w-fit"
+            type="submit"
+          >
+            Ask a Question
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+export default QuestionForm;
