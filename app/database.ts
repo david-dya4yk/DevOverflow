0ecedd0a154a3e8b19c9d93a015001bbbab297cb
david-dya4yk <daviddya4yk@mail.ru>
2025-02-24 16:47:22 +0200

    imlement user by email and id

---
+++ app/database.ts
@@ -0,0 +1,34 @@
+const tickets = [
+  {
+    id: 1,
+    name: "Fix next.js API Route bug",
+    status: "open",
+    type: "bug",
+  },
+  {
+    id: 2,
+    name: "Implement custom error handling in Next.js",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 3,
+    name: "Optimize Next.js performance",
+    status: "open",
+    type: "performance",
+  },
+  {
+    id: 4,
+    name: "Create a custom Next.js component",
+    status: "open",
+    type: "feature",
+  },
+  {
+    id: 5,
+    name: "Migrate to Next.js 13",
+    status: "open",
+    type: "feature",
+  },
+];
+
+export default tickets;
