e628cf9d7b4bc84d1979bd33a69dd465f4a58117
david-dya4yk <daviddya4yk@mail.ru>
2025-03-25 00:34:01 +0200

    implement vote action

--- types/action.d.ts
+++ types/action.d.ts
@@ -45,3 +45,13 @@ interface CreateAnswerParams {
 interface GetAnswerParams extends PaginatedSearchParams {
   questionId: string;
 }
+
+interface CreateVoteParams {
+  targetId: string;
+  targetType: "question" | "answer";
+  voteType: "upvote" | "downvote";
+}
+
+interface UpdateVoteCountParams extends CreateVoteParams {
+  change: 1 | -1;
+}
