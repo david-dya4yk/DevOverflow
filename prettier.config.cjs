3e7b9196845992e343cec853c05ac7cd863e5d08
david-dya4yk <daviddya4yk@mail.ru>
2025-10-21 15:57:00 +0300

    implement profile edit form, add prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
